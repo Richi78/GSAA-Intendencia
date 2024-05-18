19ec41f30be1c8947f93066fcd006966f6836efc
Richi78 <ricardo.rojas.carvajal@gmail.com>
2024-05-18 15:16:40 -0300

    no se que paso jaja

---
+++ interfaz/.vite/deps/@mui_material_Button.js
@@ -0,0 +1,15 @@
+"use client";
+import {
+  Button_default,
+  buttonClasses_default,
+  getButtonUtilityClass
+} from "./chunk-X7B7JCGY.js";
+import "./chunk-WXSKVWTA.js";
+import "./chunk-4JI2AD7N.js";
+import "./chunk-CEQRFMJQ.js";
+export {
+  buttonClasses_default as buttonClasses,
+  Button_default as default,
+  getButtonUtilityClass
+};
+//# sourceMappingURL=@mui_material_Button.js.map
